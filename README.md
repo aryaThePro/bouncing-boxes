6911e29f273dffaf9d777825e37cdf6e3dbc72cc
aryaThePro <65065902+aryaThePro@users.noreply.github.com>
2020-05-18 21:03:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bouncing-boxes
-using matter.js
+# Box Class
---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50);
+    box2 = new Box(230,100,50,50);
+    box3 =  new Box(200,300,50,50);
+
+    console.log(box2);
+
+    ground = new Ground(200,390,400,20);
+
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box2.display();
+    box1.display();
+    box3.display();
+    ground.display();
+}
